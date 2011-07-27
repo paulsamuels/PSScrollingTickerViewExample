1b6f774a2bf873e7c82545b0bed1fa53e979d3c8
Paul Samuels <paulio1987@gmail.com>
2011-07-27 23:48:45 +0100

    Initial Commit

---
+++ PSScrollingTickerViewExample/PSScrollingTickerViewExampleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PSScrollingTickerViewExampleAppDelegate.h
+//  PSScrollingTickerViewExample
+//
+//  Created by Paul Samuels on 27/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PSScrollingTickerViewExampleViewController;
+
+@interface PSScrollingTickerViewExampleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet PSScrollingTickerViewExampleViewController *viewController;
+
+@end
---
+++ PSScrollingTickerViewExample/PSScrollingTickerViewExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSScrollingTickerViewExampleViewController.h
+//  PSScrollingTickerViewExample
+//
+//  Created by Paul Samuels on 27/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSScrollingTickerViewExampleViewController : UIViewController
+
+@end
