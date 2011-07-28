46d4c727e6c091c9a31c087458297d9389f58b38
Paul Samuels <paulio1987@gmail.com>
2011-07-28 01:23:50 +0100

    Updated to take a font

--- PSScrollingTickerViewExample/PSScrollingTickerView.m
+++ PSScrollingTickerViewExample/PSScrollingTickerView.m
@@ -29,30 +29,41 @@ const float PSScrollingTickerViewSuperSlow  = 0.055;
 @synthesize speedModifier = _speedModifier;
 @synthesize text          = _text;
 @synthesize delay         = _delay;
+@synthesize font          = _font;
+@synthesize textColor     = _textColor;
 @synthesize scrollView    = _scrollView;
 @synthesize label         = _label;
 
+#pragma mark -
+#pragma mark Object lifecycle
+
 - (id)initWithFrame:(CGRect)frame
 {
-  return [self initWithFrame:frame text:nil delay:0 scrollSpeedModifier:PSScrollingTickerViewMedium];
+  return [self initWithFrame:frame text:nil delay:0 scrollSpeedModifier:PSScrollingTickerViewMedium font:nil];
 }
 
-- (id)initWithFrame:(CGRect)frame text:(NSString *)text delay:(CGFloat)delay scrollSpeedModifier:(CGFloat)tickerSpeedModifier
+- (id)initWithFrame:(CGRect)frame text:(NSString *)text delay:(CGFloat)delay scrollSpeedModifier:(CGFloat)tickerSpeedModifier font:(UIFont *)font
 {
   self = [super initWithFrame:frame];
   if (self) {
     _speedModifier = tickerSpeedModifier;
     _delay = delay;
     _text  = [text copy];
+    _font  = font ? font : [UIFont boldSystemFontOfSize: 20];
   }
   return self;
 }
 
 - (void)dealloc
 {
-  [_text release];
+  [_text      release];
+  [_font      release];
+  [_textColor release];
   [super dealloc];
 }
+
+#pragma mark -
+#pragma mark Configuration and Display
 
 - (void)didMoveToSuperview
 {
@@ -74,40 +85,52 @@ const float PSScrollingTickerViewSuperSlow  = 0.055;
   
   float maxAllowedWidth = self.frame.size.width;
   
-  CGSize labelSize = [self.text sizeWithFont:[UIFont boldSystemFontOfSize: 20]
+  CGSize labelSize = [self.text sizeWithFont:self.font
                       constrainedToSize:CGSizeMake(NSIntegerMax, self.frame.size.height) 
                           lineBreakMode:UILineBreakModeWordWrap];
   
+  UILabel *labelView        = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, labelSize.width, self.frame.size.height)];
+  
+  labelView.text            = self.text;
+  labelView.font            = self.font;
+  labelView.backgroundColor = [UIColor clearColor];
+  labelView.textColor       = self.textColor ? self.textColor : [UIColor whiteColor];
+  labelView.shadowColor     = [UIColor grayColor];
+  
   if (labelSize.width > maxAllowedWidth) {
-    
-    UILabel *labelView        = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, labelSize.width, self.frame.size.height)];
-    
-    labelView.text            = self.text;
-    labelView.font            = [UIFont boldSystemFontOfSize:20.0];
-    labelView.backgroundColor = [UIColor clearColor];
-    labelView.textColor       = [UIColor whiteColor];
-    labelView.shadowColor     = [UIColor grayColor];
-    
-    
+
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
     scrollView.showsHorizontalScrollIndicator = NO;
     scrollView.showsVerticalScrollIndicator   = NO;
     scrollView.contentSize                    = CGSizeMake(labelSize.width, self.frame.size.height);
     
     [scrollView addSubview:labelView];
-    self.label = labelView;
-    [labelView release]; labelView = nil;
     
     [self addSubview:scrollView];
     self.scrollView = scrollView;
     [scrollView release]; scrollView = nil;
+  } else {
+    [self addSubview:labelView];
   }
+  
+  self.label = labelView;
+  [labelView release]; labelView = nil;
+}
+
+#pragma mark -
+#pragma mark Animation and tap handling
+
+- (void)scroll
+{
+  [self scrollingTickerTapped:nil];
 }
 
 - (void)scrollingTickerTapped:(id)sender
 {
-  if ([[UIView class] respondsToSelector:@selector(animateWithDuration:delay:options:animations:completion:)]) {
-    [self animateScrollingTickerWithDelay:NO];
+  if (self.scrollView) {
+    if ([[UIView class] respondsToSelector:@selector(animateWithDuration:delay:options:animations:completion:)]) {
+      [self animateScrollingTickerWithDelay:NO];
+    }
   }
 }
 
